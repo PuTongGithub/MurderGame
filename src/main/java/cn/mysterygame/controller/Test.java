812e789cf0e653d897bafd94d2da7b93a89457a9
PuTongGithub <282549045@qq.com>
2018-02-04 09:38:48 +0800

    little change

--- src/main/java/cn/mysterygame/controller/Test.java
+++ src/main/java/cn/mysterygame/controller/Test.java
@@ -1,28 +1,13 @@
-/**************************************************************************************** 
- Copyright © 2003-2012 ZTEsoft Corporation. All rights reserved. Reproduction or       <br>
- transmission in whole or in part, in any form or by any means, electronic, mechanical <br>
- or otherwise, is prohibited without the prior written consent of the copyright owner. <br>
- ****************************************************************************************/
 package cn.mysterygame.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/** 
- * <Description> <br> 
- *  
- * @author XXX<br>
- * @version 1.0<br>
- * @taskId <br>
- * @CreateDate 2018年2月1日 <br>
- * @since V1.0<br>
- * @see cn.mysterygame.controller <br>
- */
 
 @Controller
 public class Test {
 	@GetMapping("/")
 	public String test() {
-		return "login";
+		return "index";
 	}
 }
