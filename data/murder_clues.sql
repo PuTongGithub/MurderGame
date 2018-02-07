0a30fab9a8c34b513e5fc2afaf44c06e057ab7f3
PuTongGithub <282549045@qq.com>
2018-02-07 14:01:23 +0800

    sql backup

---
+++ data/murder_clues.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: murder
+-- ------------------------------------------------------
+-- Server version	5.7.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `clues`
+--
+
+LOCK TABLES `clues` WRITE;
+/*!40000 ALTER TABLE `clues` DISABLE KEYS */;
+INSERT INTO `clues` VALUES (1,1,1,0,'死者：甄好听<br />\r\n花之都夜总会的当红花旦死在自己的房间中。<br />\r\n头部被硬物捅穿而死，凶器是细长的硬物，但在现场没有发现。<br />\r\n死者目前独居，家里只有一个女仆。<br />\r\n房间里 死者穿着凸显身材的旗袍，但没有穿鞋。<br />','',0),(2,1,2,0,'1、死者的房间：地板上有类似与圆点般的血迹，直径大约1cm，有规律地在地板上排列出现。<br />\r\n2、死者的大厅：大厅有用过的避孕套以及被脱下的睡衣和拖鞋；<br />\r\n3、死者的尸体：头部被硬物捅穿而死，凶器是细长的硬物，但在现场没有发现。死者的脸有卸妆的痕迹，但没有全部洗干净。<br />\r\n4、死者的穿着：死者穿着凸显身材的旗袍，但没有穿鞋。<br />','',0),(3,1,3,0,'1、房间的走廊：有一把看起来非常高档的折扇 。<br />\r\n2、死者的保险箱：里面有死者的辍学记录，学院是法国音乐学院，只读了半年就辍学了。<br />\r\n3、王流氓的房间：有一条黄金项链，以及其他一些金银首饰。<br />\r\n4、朱老板的随身物：一条红丝绳。<br />','',0),(4,1,4,0,'1、女仆的物品：一本写满了诅咒和埋怨死者的日记。<br />\r\n2、女仆的背景：在孤儿院长大 。<br />\r\n3、王流氓的背景：有杀人前科，家住在孤儿院附近 。<br />\r\n4、卢记者背景：有两名老父母在偏僻的乡村生活。<br />','',0),(5,1,5,0,'1、死者的书桌：有许多情书，有一半是卢记者的，有一半是朱黎的 。<br /> \r\n2、死者的房间：财物被洗劫一空；<br />\r\n3、死者的房间：藏有一份复仇计划，当年她因为父母的商行被SB帮会抢占，双亲被杀，家产被朱老板抢夺，被迫辍学。她接近朱老板就是为了复仇。 <br />\r\n3、女仆的房间：在女仆的房内发现一根有洗过痕迹的螺丝刀。<br />','',0),(6,1,6,0,'1、垃圾站：许多死者的私人财物在垃圾站被发现。<br />\r\n2、夜市摊：老板表示见过卢记者、朱黎、王流氓出现过，乞丐表示，王流氓时不时会给他们一些钱。 <br />\r\n3、SB帮：朱老板在凌晨3:00至6:00的时间段里在SB帮内，没有外出过。 <br />\r\n4、附近的居民：凌晨4点被吵醒，看到朱黎从死者家慌张地跑出来。<br />','',0),(7,1,7,0,'1、在路边摊发现撬锁用的铁锥。<br />\r\n2、门口的玄关处有一只脚跟折断的红色高跟鞋 。<br />\r\n3、大厅内有一把细长的、高档烟斗。<br />\r\n4、死者化妆台上有个沾了一些血迹的修发剪刀。<br />','',0);
+/*!40000 ALTER TABLE `clues` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-02-07 13:52:59
